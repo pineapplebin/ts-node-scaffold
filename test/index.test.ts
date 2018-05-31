f1cd0f96930b66e09436ece7350405f6593e89a2
pineapplebin <pineapplewing@126.com>
2018-05-31 16:26:56 +0800

    update tsconfig & add test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,20 @@
-import { action } from '../src'
+import { action, matrixToNumber } from '../src'
 
 test('test action', () => {
   expect(action()).toEqual(1);
 });
+
+test('matrixToNumber', () => {
+  // 100001010
+  expect(matrixToNumber([
+    [true, false, false],
+    [false, false, true],
+    [false, true, false]
+  ])).toEqual(parseInt('100001010', 2));
+
+  expect(matrixToNumber([
+    [true, true, true],
+    [true, true, true],
+    [true, true, true],
+  ])).toEqual(511);
+})
