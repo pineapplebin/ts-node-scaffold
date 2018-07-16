f956297ac802de75bd662c0bc9023d03ce6d8d7d
bensoncen <bensoncen@meijiabang.cn>
2018-07-16 15:17:54 +0800

    add sample, remove useless files

---
+++ src/sample.ts
@@ -0,0 +1,4 @@
+export default function sample(a: number, b: number): number {
+    return a + b;
+}
+
---
+++ test/sample.test.ts
@@ -0,0 +1,6 @@
+import sample from '../src/sample'
+
+test('test sample', () => {
+    expect(sample(1, 2)).toEqual(3);
+});
+
